1f9c599d02c6a01f13966cc99ef709ab7abd51af
Jose Saad <josemiguelsaad13@gmail.com>
2020-12-02 02:30:54 -0400

    Pantalla de registro
    se hizo la pantalla de registro, solo falta el boton para retroceder

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 // Stack Navigator que permite la navegación entre pantallas
 const Stack = createStackNavigator();
@@ -12,8 +13,8 @@ export default function App() {
       <Stack.Navigator>
         {/* Pantalla de Inicio de Sesión */}
         <Stack.Screen
-          name="LoginScreen"
-          component={LoginScreen}
+          name="RegisterScreen"
+          component={RegisterScreen}
           options={{
             title: "Iniciar Sesión",
             headerTitleStyle: { color: "#2d3f50" },
