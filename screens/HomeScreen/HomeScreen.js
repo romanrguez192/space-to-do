79c3c60428c975b39bc4863cac76e4a5814c0d25
miguelanggelo21@gmail.com <miguelanggelo21@gmail.com>
2020-12-05 17:25:36 -0400

    imagenes terminadas

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -49,89 +49,83 @@ const HomeScreen = (props) => {
   });
   const [loading, setLoading] = useState(true);
 
-  const getUserByID = async (id) => {
-    const dbRef = firebase.firestore().collection("users").doc(id);
-    const doc = await dbRef.get();
-    const resul = doc.data();
-    setUser({ ...resul });
-    await getImageByID(user.imageID);
-  };
+  const getUserByID = async(id) => {
+    const dbRef = firebase.firestore().collection('users').doc(id)
+    const doc = await dbRef.get()
+    const resul = doc.data()
+    setUser({...resul}) 
+    getImageByID(resul.imageID)
+    setLoading(false)
+  }
 
   const uploadImage = (uri) => {
     return new Promise((resolve, reject) => {
-      let xhr = new XMLHttpRequest();
-      xhr.onerror = reject;
+      let xhr = new XMLHttpRequest()
+      xhr.onerror = reject
       xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-          resolve(xhr.response);
+        if(xhr.readyState === 4){
+          resolve(xhr.response)
         }
-      };
-      xhr.open("GET", uri);
-      xhr.responseType = "blob";
-      xhr.send();
-    });
-  };
+      }
+      xhr.open("GET", uri)
+      xhr.responseType ="blob"
+      xhr.send()
+    }) 
+  }
 
-  const updateUser = async () => {
-    const dbRef = firebase.firestore().collection("users").doc(props.extraData);
+  const updateUser = async(imageID) => {
+    const dbRef = firebase.firestore().collection('users').doc(props.extraData)
     await dbRef.set({
       name: user.name,
       email: user.email,
       gender: user.gender,
       id: user.id,
-      imageID: user.imageID,
+      imageID,
       username: user.username,
-    });
-  };
+    })
+    getImageByID(imageID)
+  }
 
-  const pickImage = async () => {
-    const permission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-    if (permission.granted) {
+  const pickImage = async() => {
+    const permission = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+    if(permission.granted){
       const resulImage = await ImagePicker.launchImageLibraryAsync({
         allowsEditing: true,
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
         aspect: [3, 3],
         quality: 1,
-      });
+      })
 
-      if (!resulImage.cancelled) {
-        const uuid = uuid4();
-        setUser({ ...user, imageID: uuid });
-        uploadImage(resulImage.uri)
-          .then((resolve) => {
-            const ref = firebase.storage().ref().child(`images/${uuid}.jpg`);
-            ref
-              .put(resolve)
-              .then((resolve) => {
-                Alert.alert("Imagen subida correctamente");
-              })
-              .catch((err) => {
-                Alert.alert("No se pudo cargar la imagen intente de nuevo");
-              });
+      if(!resulImage.cancelled) {
+        const uuid = uuid4()
+        setUser({...user, imageID: uuid })
+        uploadImage(resulImage.uri).then( resolve => {
+          const ref = firebase.storage().ref().child(`images/${uuid}.jpg`)
+          ref.put(resolve).then(resolve => {
+            updateUser(uuid)
+            Alert.alert("Imagen subida correctamente")
+          }).catch(err => {
+            Alert.alert("No se pudo cargar la imagen intente de nuevo")
           })
-          .catch((err) => {
-            console.log(err);
-          });
-        updateUser();
+          
+        }).catch(err => {
+          console.log(err)
+        })
       }
     }
-  };
+  }
 
-  const getImageByID = async (id) => {
-    if (id !== "") {
-      firebase
-        .storage()
-        .ref(`images/${id}.jpg`)
-        .getDownloadURL()
-        .then((resolve) => {
-          setImage({ uri: resolve });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+  const getImageByID = async(id) => {
+    if(id !== ''){
+      firebase.storage().ref(`images/${id}.jpg`).getDownloadURL().then(resolve => {
+        setImage({uri: resolve})
+      }).catch(err => {
+        console.log(err)
+      })
+    }else {
+      setImage({uri: ""})  
     }
-    setLoading(false);
-  };
+  }
 
   const getTitleByName = (name) => {
     let resul = name.toUpperCase().charAt(0);
@@ -145,6 +139,17 @@ const HomeScreen = (props) => {
         <ActivityIndicator size="large" color="#9e9e9e" />
       </View>
     );
+  }
+
+  const deleteImage = () => {
+    if(!(user.imageID === "")){
+      const imageRef = firebase.default.storage().ref(`images/${user.imageID}.jpg`)
+      imageRef.delete().then(() => {
+        console.log("Deleted")
+        setUser({...user, imageID: ""})
+        updateUser("")
+      }).catch(err => console.log("Cannot be deleted: " + err))
+    }
   }
 
   const colorHEX = () => {
@@ -283,25 +288,37 @@ const HomeScreen = (props) => {
               borderBottomWidth: 1,
             }}
           />
-          {/* EJEMPLO PARA CARGAR LA IMAGEN */}
-          <ListItem key={user.id}>
-            {user.imageID === "" ? (
-              <Avatar
-                size="medium"
-                title={getTitleByName(user.name)}
-                containerStyle={{ backgroundColor: colorHEX() }}
-              />
-            ) : (
-              <Avatar size="medium" source={{ uri: image.uri }} />
-            )}
-
+            {/* EJEMPLO PARA CARGAR LA IMAGEN */}
+            <ListItem key={user.id}>
+              {
+                
+                user.imageID === "" 
+                ? 
+                <Avatar 
+                  rounded
+                  size="medium" 
+                  title={getTitleByName(user.name)}
+                  containerStyle={{backgroundColor: colorHEX()}}
+                />
+                :
+                <Avatar 
+                  rounded
+                  size="medium" 
+                  source={{uri: image.uri}}
+                />
+              }
+              
             <ListItem.Content>
-              <ListItem.Title>{user.imageID}</ListItem.Title>
-              <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+              
+              <ListItem.Title>{user.name}</ListItem.Title>
+              <ListItem.Subtitle >{user.email}</ListItem.Subtitle>
             </ListItem.Content>
-            <Icon name="add" onPress={pickImage} />
-          </ListItem>
-          {/* FIN DE EJEMPLO */}
+            <Icon name="add" onPress={pickImage}/>
+            <View>
+              <Button title="delete" onPress={deleteImage}></Button>
+            </View>
+            </ListItem>
+            {/* FIN DE EJEMPLO */}
 
           <View>{/* TODO: AGREGAR LA LISTA DE TAREAS POR CULMINAR */}</View>
         </ScrollView>
