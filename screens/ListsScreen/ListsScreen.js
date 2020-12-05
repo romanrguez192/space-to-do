411858d8adbb630c88149b299cf1d3c8da8886de
cuaulma <monacuaulma@gmail.com>
2020-12-05 15:27:24 -0400

    ola

--- screens/ListsScreen/ListsScreen.js
+++ screens/ListsScreen/ListsScreen.js
@@ -1,12 +1,56 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Text, View } from "react-native";
+import { Text, FlatList, View, TouchableOpacity } from "react-native";
+import { Input, Icon, ListItem, Avatar, Overlay } from "react-native-elements";
+
 import styles from "./styles";
 import { firebase } from "../../firebase/config";
 
 const ListsScreen = () => {
+  const [list, setList] = useState({
+    title: "",
+    subtitle: "",
+    theme: "",
+    listItem: "",
+  });
+  const [visible, setVisible] = useState(false);
+
+  const handleChangeText = (name, value) => {
+    setList({ ...list, [name]: value });
+  };
+
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  };
+  const createList = () => {};
   return (
     <View>
-      <Text>Listas</Text>
+      <TouchableOpacity
+        style={styles.buttonAddStyle}
+        onPress={() => toggleOverlay()}
+      >
+        <Icon
+          type="font-awesome"
+          name="plus"
+          size={30}
+          color="#FF8C00"
+          backgroundColor="#FF8C0050"
+          alignItems="center"
+          style={styles.iconStyle}
+        />
+      </TouchableOpacity>
+      <Overlay isVisible={visible} onBackdropPress={toggleOverlay}>
+        <Input
+          placeholder="Ej: Compras"
+          label="Título"
+          onChangeText={(value) => handleChangeText("title", value)}
+        />
+        <TouchableOpacity style={styles.button} onPress={createList()}>
+          <Text style={styles.buttonText}>Crear lista</Text>
+        </TouchableOpacity>
+      </Overlay>
+      <TouchableOpacity>
+        <FlatList />
+      </TouchableOpacity>
     </View>
   );
 };
--- screens/ListsScreen/styles.js
+++ screens/ListsScreen/styles.js
@@ -5,4 +5,28 @@ const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 // Estilos
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  button: {
+    backgroundColor: "#e54e42",
+    paddingHorizontal: 80,
+    paddingVertical: 15,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  buttonAddStyle: {
+    position: "absolute",
+    marginTop: windowHeight * 0.8,
+    marginLeft: windowWidth * 0.75,
+  },
+  iconStyle: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#fff",
+  },
+});
