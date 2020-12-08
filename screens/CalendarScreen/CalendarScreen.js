a61ed3711502d9e5c80a7d48bd9ec80ff5a81e27
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2020-12-07 22:07:47 -0400

    Agregado el Header al Calendario
    Solo eso, para Moni

--- screens/CalendarScreen/CalendarScreen.js
+++ screens/CalendarScreen/CalendarScreen.js
@@ -2,11 +2,35 @@ import React, { useState, useRef, useEffect } from "react";
 import { Text, View } from "react-native";
 import styles from "./styles";
 import { firebase } from "../../firebase/config";
+import { Appbar, DefaultTheme } from "react-native-paper";
 
-const CalendarScreen = () => {
+const CalendarScreen = (props) => {
+  const CustomHeader = () => {
+    return (
+      <Appbar.Header
+        theme={{
+          ...DefaultTheme,
+          roundness: 2,
+          colors: {
+            ...DefaultTheme.colors,
+            primary: "#e54e42",
+          },
+        }}
+      >
+        <Appbar.Action
+          icon="menu"
+          onPress={() => props.navigation.toggleDrawer()}
+        />
+        <Appbar.Content title="Calendario" />
+        <Appbar.Action icon="dots-vertical" />
+      </Appbar.Header>
+    );
+  };
+
   return (
-    <View style={{flex:1 ,alignItems: 'center', justifyContent:'center'}}>
-      <Text style={{fontWeight: 'bold'}}>Calendario</Text>
+    <View>
+      <CustomHeader />
+      <Text style={{ fontWeight: "bold" }}>Calendario</Text>
     </View>
   );
 };
