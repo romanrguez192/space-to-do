a5e28622b54cec49f285031ae6ce66987a36401f
miguelanggelo21@gmail.com <miguelanggelo21@gmail.com>
2020-12-14 22:27:48 -0400

    taskscreen

--- screens/TasksScreen/TasksScreen.js
+++ screens/TasksScreen/TasksScreen.js
@@ -4,7 +4,7 @@ import ContentLoader, {
   FacebookLoader,
   InstagramLoader,
 } from "react-native-easy-content-loader";
-import { Appbar, DefaultTheme, Checkbox } from "react-native-paper";
+import { Appbar, DefaultTheme, Checkbox, List, IconButton } from "react-native-paper";
 import React, {
   useState,
   useRef,
@@ -21,6 +21,7 @@ import {
   Alert,
   SafeAreaView,
   KeyboardAvoidingView,
+  StyleSheet,
   Image,
 } from "react-native";
 import { Input, Icon, ListItem, Avatar, Overlay } from "react-native-elements";
@@ -32,18 +33,18 @@ import Accordion from "react-native-collapsible/Accordion";
 const TasksScreen = (props) => {
   const [loading, setLoading] = useState(true);
   const [tasks, setTasks] = useState(null);
-
+  const Theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: props.list.theme,
+    },
+  }
   const CustomHeader = () => {
     return (
       <Appbar.Header
-        theme={{
-          ...DefaultTheme,
-          roundness: 2,
-          colors: {
-            ...DefaultTheme.colors,
-            primary: props.list.theme,
-          },
-        }}
+        theme={Theme}
       >
         <Appbar.BackAction
           onPress={() => props.navigation.navigate("Mis Listas")}
@@ -87,8 +88,7 @@ const TasksScreen = (props) => {
           snapshot.docs.forEach((doc) => {
             data.push({
               id: doc.id,
-              title: doc.data().title,
-              content: doc.data().description,
+              ...doc.data(),
             });
           });
           if (data.length === 0) setTasks(null);
@@ -108,69 +108,97 @@ const TasksScreen = (props) => {
     });
   };
 
-  const [config, setConfig] = useState({
-    activeS: [],
-    collapsed: true,
-    multipleSelect: false,
-  });
+  // const [config, setConfig] = useState({
+  //   activeS: [],
+  //   collapsed: true,
+  //   multipleSelect: false,
+  // });
 
-  const toggleExpanded = () => {
-    setConfig({ ...config, collapsed: !config.collapsed });
-  };
+  // const toggleExpanded = () => {
+  //   setConfig({ ...config, collapsed: !config.collapsed });
+  // };
 
-  const setSections = (sections) => {
-    setConfig({
-      ...config,
-      activeS: sections.includes(undefined) ? [] : sections,
-    });
-  };
+  // const setSections = (sections) => {
+  //   setConfig({
+  //     ...config,
+  //     activeS: sections.includes(undefined) ? [] : sections,
+  //   });
+  // };
 
-  const renderContent = (section, _, isActive) => {
+  // const renderContent = (section, _, isActive) => {
+  //   return (
+  //     <Animatable.View
+  //       duration={400}
+  //       style={[styles.content, isActive ? styles.active : styles.inactive]}
+  //       transition="color"
+  //     >
+  //       <Animatable.Text
+  //         style={{ marginLeft: 46, color: "gray", fontSize: 15 }}
+  //         duration={400}
+  //         animation={isActive ? "slideInDown" : undefined}
+  //       >
+  //         {section.content}
+  //       </Animatable.Text>
+  //     </Animatable.View>
+  //   );
+  // };
+
+  const [titleStyles, setTitleStyles] = useState({textDecorationLine:'line-through'})
+
+  // const titleStyles = (done) => {
+  //   return (done)? {textDecorationLine:"line-through"} : undefined
+  // }
+
+  const setDone = async(item) => {
+    const dbRef = firebase.default.firestore().collection('tasks').doc(item.id)
+    await dbRef.set({
+      ...item,
+      done: !(item.done),
+    })
+  }
+
+  const getDateFromString = (limit) => {
+    const resul= new Date(limit * 1000);
+    return resul.getDate()  + "/" + (resul.getMonth() + 1) + "/" +  (resul.getFullYear() - 1969 );
+  }
+
+  const renderList = (items) => {
     return (
-      <Animatable.View
-        duration={400}
-        style={[styles.content, isActive ? styles.active : styles.inactive]}
-        transition="color"
-      >
-        <Animatable.Text
-          style={{ marginLeft: 46, color: "gray", fontSize: 15 }}
-          duration={400}
-          animation={isActive ? "slideInDown" : undefined}
-        >
-          {section.content}
-        </Animatable.Text>
-      </Animatable.View>
-    );
-  };
-
-  const renderHeader = (section, _, isActive) => {
-    return (
-      <Animatable.View
-        duration={400}
-        style={[styles.header, isActive ? styles.active : styles.inactive]}
-        transition="backgroundColor"
-      >
-        <Checkbox></Checkbox>
-        <Text style={styles.headerText}>{section.title}</Text>
-      </Animatable.View>
-    );
-  };
-
-  const renderList = (item) => {
-    return (
-      <View style={styles.shadow}>
-        <Accordion
-          activeSections={config.activeS}
-          sections={item}
-          touchableComponent={TouchableOpacity}
-          expandMultiple={false}
-          renderHeader={renderHeader}
-          renderContent={renderContent}
-          duration={400}
-          onChange={setSections}
-          sectionContainerStyle={{ marginBottom: 20, marginTop: 20 }}
-        />
-      </View>
+      <List.Section title="Tareas" >
+        {
+          items.map((item) => {
+            return(
+              <View style={{flex:1, flexDirection:'row'}}>
+                <View style={{flex:.15, alignItems:'center', padding:0, margin:0}}>
+                  <Checkbox.Item 
+                    status={(item.done? "checked" : "unchecked")} 
+                    onPress={() => setDone(item)} color={props.list.theme}
+                    style={{padding:0, margin:0}}
+                    />
+                </View>
+                <View style={{flex:.9}}>
+                  <List.Accordion
+                    title={item.title}
+                    titleStyle={{fontWeight:'bold'},(item.done)? {textDecorationLine:'line-through'} : undefined}
+                    style={{paddingBottom:0}}
+                    theme={Theme}
+                  >
+                    <TouchableOpacity activeOpacity={0.7} /*onLongPress={}*/>
+                      <List.Item 
+                        style={{paddingTop:0}}
+                        description={item.description}
+                        descriptionStyle={(item.done)? {textDecorationLine:'line-through'} : undefined}
+                        title={getDateFromString(item.limit)}
+                        titleStyle={{fontSize:12}}
+                      />
+                    </TouchableOpacity>
+                  </List.Accordion>
+                </View>
+              </View>
+            )
+          })
+        }
+      </List.Section>
     );
   };
 
